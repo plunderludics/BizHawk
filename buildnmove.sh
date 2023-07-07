2ee633f231312f8ea83e4db9d76f67127b017911
hello <hello>
2023-07-07 13:55:00 -0400

    don't add unused dlls to unity project

--- buildnmove.sh
+++ buildnmove.sh
@@ -1,2 +1,9 @@
+targetDir="../unity-hawk/Packages/org.plunderludics.UnityHawk/BizHawk/dll"
+
 dotnet build . -c Release -p:UnityHawk=true &&
-cp output/dll/* ../unity-hawk/Packages/org.plunderludics.UnityHawk/BizHawk/dll
+cp output/dll/* $targetDir
+
+# We don't need these, and they cause problems for building, so delete them:
+rm $targetDir/EmuHawk.odb $targetDir/SlimDx.dll $targetDir/BizHawk.Bizware.DirectX.*
+# This one we need but it's registered as a dependency in the unity project:
+rm $targetDir/Newtonsoft.Json.dll

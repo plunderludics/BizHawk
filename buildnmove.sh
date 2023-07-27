54da89fe2e8b1c8316459260b06cf26efe1517e1
hello <hello>
2023-07-27 01:12:09 -0400

    improve build script

--- buildnmove.sh
+++ buildnmove.sh
@@ -1,6 +1,13 @@
-targetDir="../unity-hawk/Packages/org.plunderludics.UnityHawk/BizHawk"
+packageDir="../unity-hawk/Packages/org.plunderludics.UnityHawk"
+bizhawkDir="$packageDir/BizHawk~"
+
+mkdir -p $bizhawkDir/dll $bizhawkDir/gamedb
 
 dotnet build . -c Release -p:UnityHawk=true &&
-cp output/EmuHawk.exe $targetDir/EmuHawk.exe &&
-cp output/dll/* $targetDir/dll
-
+# for dlls that are needed by unity, make a second copy outside of the BizHawk~ dir:
+cp output/dll/BizHawk.Plunderludics.dll $packageDir/Plugins/BizHawk.Plunderludics.dll &&
+cp output/dll/BizHawk.UnityHawk.dll $packageDir/Plugins/BizHawk.UnityHawk.dll &&
+# everything else is only used by the bizhawk exe itself:
+cp output/EmuHawk.exe $bizhawkDir/EmuHawk.exe &&
+cp output/dll/* $bizhawkDir/dll &&
+cp output/gamedb/* $bizhawkDir/gamedb

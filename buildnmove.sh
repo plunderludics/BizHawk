dd5273fb21bc1821a5f8b9ac191934544238665a
iatenothingbutriceforthreedays <xxx@x.com>
2023-07-28 15:16:38 +0900

    update buildnmove.sh

--- buildnmove.sh
+++ buildnmove.sh
@@ -4,11 +4,18 @@ bizhawkDir="$packageDir/BizHawk~"
 
 mkdir -p $bizhawkDir/dll $bizhawkDir/gamedb
 
-dotnet build src/BizHawk.Client.Headless/ -c Release -p:UnityHawk=true &&
+# For Mac:
+#buildProject=src/BizHawk.Client.Headless/
+#exeName=EmuHawk-Headless.exe
+
+buildProject=src/BizHawk.Client.EmuHawk/
+exeName=EmuHawk.exe
+
+dotnet build $buildProject -c Release -p:UnityHawk=true &&
 # for dlls that are needed by unity, make a second copy outside of the BizHawk~ dir:
 cp output/dll/Plunderludics.dll $packageDir/Plugins/Plunderludics.dll &&
 cp output/dll/Plunderludics.UnityHawk.dll $packageDir/Plugins/Plunderludics.UnityHawk.dll &&
 # everything else is only used by the bizhawk exe itself:
-cp output/EmuHawk-Headless.exe $bizhawkDir/EmuHawk-Headless.exe &&
+cp output/$exeName $bizhawkDir/$exeName &&
 cp output/dll/* $bizhawkDir/dll &&
 cp output/gamedb/* $bizhawkDir/gamedb

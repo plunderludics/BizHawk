aebacc58fba10257b2b32bdee8f164f34ac08656
iatenothingbutriceforthreedays <darwinvickers@gmail.com>
2025-11-22 11:07:19 +0900

    remove noisy argparser output

--- src/BizHawk.Client.Common/ArgParser.cs
+++ src/BizHawk.Client.Common/ArgParser.cs
@@ -173,7 +173,7 @@ namespace BizHawk.Client.Common
 		public static int? ParseArguments(out ParsedCLIFlags parsed, string[] args)
 		{
 			parsed = default;
-			if (args.Length is not 0) Console.Error.WriteLine($"parsing command-line flags: {string.Join(" ", args)}");
+			// if (args.Length is not 0) Console.Error.WriteLine($"parsing command-line flags: {string.Join(" ", args)}");
 			var result = Parser.Parse(args);
 			if (result.Errors.Count is not 0)
 			{

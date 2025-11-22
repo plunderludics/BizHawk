801e18181593124c780aac4ef1f524cefb7a4a46
iatenothingbutriceforthreedays <darwinvickers@gmail.com>
2025-11-22 09:34:36 +0900

    remove unused Headless project (attempt at mac support)

--- src/BizHawk.Client.Headless/DialogParent.cs
+++
@@ -1,7 +0,0 @@
-using BizHawk.Client.Common;
-
-namespace BizHawk.Client.Headless {
-class DialogParent : IDialogParent {
-    public IDialogController DialogController { get; } = new DialogController();
-}
-}

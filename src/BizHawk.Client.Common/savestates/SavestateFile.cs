9a1ccfa1c062064148a65f53700c815907e93226
iatenothingbutriceforthreedays <darwinvickers@gmail.com>
2025-11-22 12:08:57 +0900

    remove check for outdated savestates (but, many old savestates are still incompatible due to core changes

--- src/BizHawk.Client.Common/savestates/SavestateFile.cs
+++ src/BizHawk.Client.Common/savestates/SavestateFile.cs
@@ -180,18 +180,18 @@ namespace BizHawk.Client.Common
 						}
 					}
 				});
-				if (loadedSyncSettings is null
-					|| !ConfigService.SaveWithType(_settable.GetSyncSettings())
-						.Equals(loadedSyncSettings, StringComparison.Ordinal))
-				{
-					dialogParent.ModalMessageBox(
-						loadedSyncSettings is null
-							? "This savestate doesn't contain sync settings, so it must be from an older version.\nLoadstate cancelled."
-							: "This savestate was made with a different core or different sync settings.\nLoadstate cancelled.",
-						"Savestate sync settings mismatch",
-						EMsgBoxIcon.Info);
-					return false;
-				}
+				// if (loadedSyncSettings is null
+				// 	|| !ConfigService.SaveWithType(_settable.GetSyncSettings())
+				// 		.Equals(loadedSyncSettings, StringComparison.Ordinal))
+				// {
+				// 	dialogParent.ModalMessageBox(
+				// 		loadedSyncSettings is null
+				// 			? "This savestate doesn't contain sync settings, so it must be from an older version.\nLoadstate cancelled."
+				// 			: "This savestate was made with a different core or different sync settings.\nLoadstate cancelled.",
+				// 		"Savestate sync settings mismatch",
+				// 		EMsgBoxIcon.Info);
+				// 	return false;
+				// }
 			}
 
 			// Movie timeline check must happen before the core state is loaded
